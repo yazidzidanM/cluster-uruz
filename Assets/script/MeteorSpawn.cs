9b3505a03225944794b071f675ca63be53b863b4
Yazid Zidan <zidanotm@gmail.com>
2026-08-28 22:28:11 +0700

    fixing bird patrol

--- Assets/script/MeteorSpawn.cs
+++ Assets/script/MeteorSpawn.cs
@@ -18,7 +18,7 @@ public class MeteorSpawn : MonoBehaviour
 
     [Header("Patrol Settings")]
     public float moveSpeed = 5f;
-    public float patrolDistance = 10f;
+    public float patrolDistance = 15f;
 
     private Camera cam;
 
